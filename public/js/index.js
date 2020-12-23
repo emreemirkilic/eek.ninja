6e7764bcad854d61897c365883867d6a3cc665b9
walkeriyecrew <61476229+walkeriyecrew@users.noreply.github.com>
2020-12-23 16:59:56 +0300

    23.12.2020 04:59 PM"

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+var copyright = document.getElementById('copyright');
+function abdullah(){
+    if(copyright.innerHTML === "Emre Emir Kılıç"){
+        copyright.innerHTML = "Blockcu Abdullah";
+    }
+    else if(copyright.innerHTML === "Blockcu Abdullah"){
+        copyright.innerHTML = "Emre Emir Kılıç";
+    }
+
+}
